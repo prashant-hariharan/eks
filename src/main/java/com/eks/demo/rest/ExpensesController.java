c599028954202142885833d514897a39e4b040d6
prashant <prashant.hariharan@externals.adidas.com>
2024-04-22 10:32:29 +0200

    Updating deployment.yaml and adding logging

--- src/main/java/com/eks/demo/rest/ExpensesController.java
+++ src/main/java/com/eks/demo/rest/ExpensesController.java
@@ -5,6 +5,8 @@ import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,33 +22,41 @@ import com.eks.demo.dto.ExpenseDTO;
 @RequestMapping("/expenses")
 public class ExpensesController {
 
+	private final Logger logger = LoggerFactory.getLogger(getClass());
+
 	Map<String, ExpenseDTO> expenses = new ConcurrentHashMap<String, ExpenseDTO>();
 
 	@GetMapping("/{expenseId}")
 	public ExpenseDTO getExpensById(@PathVariable String expenseId) {
+		logger.info("Finding expense with id {}", expenseId);
 		if (expenses.containsKey(expenseId)) {
 			return expenses.get(expenseId);
 		}
+
 		throw new RuntimeException("Expense with id " + expenseId + " not found");
 	}
 
 	@PostMapping("")
 	public ExpenseDTO createExpense(@RequestBody ExpenseDTO expense) {
+
 		String expenseID = UUID.randomUUID().toString();
 		expense.setId(expenseID);
 		expense.setDateTime(LocalDateTime.now());
 		expenses.put(expenseID, expense);
+		logger.info("Created expense with id {}", expenseID);
 		return expense;
 	}
 
 	@PutMapping("/{expenseId}")
 	public ExpenseDTO updateExpense(@PathVariable String expenseId, @RequestBody ExpenseDTO expense) {
+		logger.info("Updating expense with id {}", expenseId);
 		expenses.put(expense.getId(), expense);
 		return expense;
 	}
 
 	@DeleteMapping("/{expenseId}")
 	public void updateExpense(@PathVariable String expenseId) {
+		logger.info("Deleting expense with id {}", expenseId);
 		expenses.remove(expenseId);
 	}
 }
